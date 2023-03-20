fe3442a6e08bb2e078bc5eca7bac5c36fd51a663
Alphasec <95212748+alphasecio@users.noreply.github.com>
2023-03-20 11:33:25 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,20 @@
-import streamlit as st
+import os, streamlit as st
 
-st.set_page_config(page_title="Streamlit App", page_icon=":rocket:")
-st.header("Streamlit App")
+# Authenticate with the Tradier API
+access_token = os.environ["TRADIER_TOKEN"]
+tradier = TradierAPI(access_token, "sandbox")
+
+# Define a function to get stock quotes
+def get_stock_quote(symbol):
+    quotes = tradier.get_quotes(symbol)
+    for quote in quotes:
+        if quote.symbol == symbol:
+            return quote.last
+    return None
+
+# Set up the Streamlit app
+st.title("Stocks App")
+symbol = st.text_input("Enter a stock symbol", "AAPL")
+if st.button("Get Quote"):
+    quote = get_stock_quote(symbol)
+    st.write(f"The last price for {symbol} is {quote}")
